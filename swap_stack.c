95bd0781b760da4549c1e1f421ea8fe99c661258
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-03-25 05:12:53 +0100

    Function definition for the swap operation

---
+++ swap_stack.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * swap_stack : Swaps the top two element of the stack
+ *
+ * @s: Pointer to the top of the stack
+ * @line_number: Line number of the monty bytecode being executed
+ *
+ * Return: Nothing
+ */
+void swap_stack(stack_t **s, unsigned int line_number)
+{
+	int swp;
+
+	swp = 0;
+	if ((*s)->next == (void *) 0)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	swp = (*s)->n;
+	(*s)->n = (*s)->next->n;
+	(*s)->next->n = swp;
+	flush_line();
+}
