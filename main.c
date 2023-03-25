1542d14a27ddbfd99dd9a4db01aaf4446bf97cad
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-03-26 00:58:31 +0100

    Now monty_b struct to handle the monty bytecode

--- main.c
+++ main.c
@@ -4,40 +4,42 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
-char *monty_bytecode = (void *) 0;
+monty_b *code_m = (void *) 0;
 
 /**
  * run_monty - Parses the monty bytecode and makes the necessary function calls
  *
- * @bytecode: Input string containing the monty bytecode
+ * @monty: Pointer to monty struct containing the monty bytecod to run
  * @stack: A pointer to the stack data structure
  *
  * Return: Nothing
  */
-void run_monty(char **bytecode, stack_t **stack)
+void run_monty(monty_b **monty, stack_t **stack)
 {
+	void (*func)(stack_t **, unsigned int);
 	int line_no;
 	char *tok;
-	void (*func)(stack_t **, unsigned int);
 
 	line_no = 1;
 	tok = (void *) 0;
 	do {
-		tok = linetoken(bytecode, " \n\0");
-		if (tok == (void *) 0)
+		linetoken(monty, " \n\0");
+		if ((*monty)->len == -2)
 		{
 			line_no++;
 			continue;
 		}
-		func = get_op_func(tok);
+		func = get_op_func(monty);
 		if (func == (void *) 0)
 		{
+			tok = _memcpy(tok, monty);
 			fprintf(stderr, "L%d: unknown instruction %s\n", line_no, tok);
+			free(tok);
 			exit(EXIT_FAILURE);
 		}
 		func(stack, line_no);
 		line_no++;
-	} while (monty_bytecode[0] != '\0');
+	} while ((*monty)->start != -1);
 }
 
 /**
@@ -52,7 +54,6 @@ int main(int argc, char **argv)
 {
 	int fd_monty;
 	stack_t *stack;
-	char *t_free;
 
 	stack = (void *) 0;
 	if (argc != 2)
@@ -66,10 +67,15 @@ int main(int argc, char **argv)
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	monty_bytecode = read_file(argv[1]);
-	t_free = monty_bytecode;
+	code_m = malloc(sizeof(*code_m));
+	code_m->bytecode = read_file(argv[1]);
+	code_m->start = 0;
+	code_m->len = -1;
+	code_m->pstart = 0;
+	code_m->plen = -1;
+	code_m->arg = -1;
 
-	run_monty(&monty_bytecode, &stack);
-	free(t_free);
+	run_monty(&code_m, &stack);
+	free_stack(&stack);
 	return (EXIT_SUCCESS);
 }
