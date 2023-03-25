0735f372e0c24b1967b1dd0aef8af73f74e57670
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-03-25 05:14:26 +0100

    Function definition for the nop operation

---
+++ nop_stack.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+ * nop_stack - Does nothing
+ *
+ * @s: Pointer to the stack
+ * @line_number: Line number of the monty bytecode being executed
+ *
+ * Return: Nothing
+ */
+void nop_stack(stack_t **s, unsigned int line_number)
+{
+	if (line_number >= 1)
+		*s = (void *) 0;
+	flush_line();
+}
