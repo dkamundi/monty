a65d9c1c4cffdf07724c88d7c34296f53a646d49
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-03-26 02:35:56 +0100

    Flushed line at the end of function

--- add_stack.c
+++ add_stack.c
@@ -12,12 +12,19 @@
  */
 void add_stack(stack_t **s, unsigned int line_number)
 {
+	stack_t *tmp;
+
+	tmp = (void *) 0;
 	if ((*s) == (void *) 0 || (*s)->next == (void *) 0)
 	{
+		free_all();
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
+	tmp = *s;
 	(*s)->next->n += (*s)->n;
 	(*s)->next->prev = (void *) 0;
 	*s = (*s)->next;
+	free(tmp);
+	flush_line();
 }
