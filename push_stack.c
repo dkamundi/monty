4a979e0ad07267368f3ce10f2625367085c8d889
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-03-26 01:05:19 +0100

    Now uses monty_b struct

--- push_stack.c
+++ push_stack.c
@@ -16,13 +16,19 @@ void push_stack(stack_t **stack, unsigned int line_number)
 	int val;
 	char *tok;
 
+	tok = (void *) 0;
 	psh = malloc(sizeof(*psh));
 	if (psh == (void *) 0)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
-	tok = linetoken(&monty_bytecode, " \n");
+	if (code_m->arg > 0)
+	{
+		code_m->start = code_m->arg;
+		linetoken(&code_m, " \n");
+		tok = _memcpy(tok, &code_m);
+	}
 	if (tok == (void *) 0 || !validint(tok))
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
@@ -39,5 +45,6 @@ void push_stack(stack_t **stack, unsigned int line_number)
 		psh->next = *stack;
 	}
 	*stack = psh;
+	free(tok);
 	flush_line();
 }
