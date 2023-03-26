8698efa93e6724822a3e73bb578e99ae6a53c434
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-03-26 01:47:02 +0100

    Freed all previous nodes

--- pop_stack.c
+++ pop_stack.c
@@ -12,17 +12,24 @@
  */
 void pop_stack(stack_t **s, unsigned int line_number)
 {
+	stack_t *tmp;
+
+	tmp = (void *) 0;
 	if (*s == (void *) 0)
 	{
 		fprintf(stderr, "L%u: can't pop an empty stack", line_number);
 		exit(EXIT_FAILURE);
 	}
+	tmp = *s;
 	if ((*s)->next != (void *) 0)
 	{
 		*s = (*s)->next;
 		(*s)->prev = (void *) 0;
 	} else
+	{
 		*s = (void *) 0;
+	}
+	free(tmp);
 	flush_line();
 }
 
